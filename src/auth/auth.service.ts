12e3b3d4dc502c6f98e0909144f12c5715581035
Dzung Nguyen <dzung_min@yahoo.com>
2022-08-18 16:25:09 +0700

    implements auth with jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,22 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
+import { Response } from 'express';
+import { IJwtPayload } from 'src/interfaces/jwt-payload.interface';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async getValidatedUser(email: string, password: string) {
     try {
@@ -21,6 +30,10 @@ export class AuthService {
     }
   }
 
+  getUserFromJwtPayload(payload: IJwtPayload) {
+    return this.usersService.findOneByID(payload.userId);
+  }
+
   async checkValidPassword(plaintextPassword: string, hashedPassword: string) {
     const isMatch = await compare(plaintextPassword, hashedPassword);
     if (!isMatch) throw new BadRequestException("Password doesn't match");
@@ -28,5 +41,15 @@ export class AuthService {
 
   signup(signupData: CreateUserDto) {
     return this.usersService.create(signupData);
+  }
+
+  setAuthCookie(user: User, res: Response) {
+    const payload: IJwtPayload = { userId: user.id };
+    const token = this.jwtService.sign(payload);
+    res.cookie('Authentication', token, {
+      httpOnly: true,
+      maxAge: Number(this.configService.get('JWT_EXPIRATION_TIME')) * 1000,
+      signed: true,
+    });
   }
 }
