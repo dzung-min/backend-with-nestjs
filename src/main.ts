eb278bdfb48428cd74855f1c68afad937ec9cf02
Dzung Nguyen <dzung_min@yahoo.com>
2022-08-19 01:13:55 +0700

    enable cors and using helmet for better security

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.enableCors();
+  app.use(helmet());
   app.use(cookieParser(configService.get('COOKIE_SECRET')));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
