c8b62085152789d5efb0ebb0f77ae0b34b19da78
Dzung Nguyen <dzung_min@yahoo.com>
2022-08-19 17:30:54 +0700

    change database from Postgres to SQLite3

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,22 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PW'),
-        database: configService.get('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      synchronize: true,
+      autoLoadEntities: true
+    })
   ],
 })
 export class DatabaseModule {}
